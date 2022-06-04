91ec9bae3b82d7ab9828aef791f63f898ab96c0d
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2022-06-03 22:49:03 -0700

    home design was changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
       colors:{
         'portfolio-blue-dark':'rgb(2, 2, 18)',
         'portfolio-blue-alpha-400':'rgba(3, 2, 26, 0.400)'
+      },
+      spacing:{
+        'portfolio-30':'30rem',
+        'portfolio-27':'27rem'
       }
     },
   },
