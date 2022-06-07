fcdf419cd7c3b6f9581f393c27f0a1fe7bd4ec7a
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2022-06-06 19:03:14 -0700

    home unit test was added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -14,18 +18,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'portfolio'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio app is running!');
   });
 });
--- src/app/modules/home/home.component.spec.ts
+++ src/app/modules/home/home.component.spec.ts
@@ -1,16 +1,22 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UserInfoMock } from 'src/app/mocks/userInfoMock';
+import { HomeService } from 'src/app/services/homeService/home.service';
 
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let homeService: HomeService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      schemas:[CUSTOM_ELEMENTS_SCHEMA],
       declarations: [ HomeComponent ]
     })
     .compileComponents();
+    homeService = TestBed.inject(HomeService);
   });
 
   beforeEach(() => {
@@ -21,5 +27,12 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Home component', ()=>{
+    it('ngOnInit', ()=>{
+      component.ngOnInit()
+      expect(component.userInfo).toEqual(UserInfoMock)
+    });
   });
 });
