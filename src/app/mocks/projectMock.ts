abfc72a349ce6a356c4a4cb5d38f8c0f198cb674
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2022-10-12 21:49:50 -0700

    create project mock

---
+++ src/app/mocks/projectMock.ts
@@ -0,0 +1,33 @@
+import { Project } from "../models/project";
+
+export const Projects: Project[] = [
+    {
+        id: "1",
+        name: "Solinsa Website",
+        description: "",
+        image: "",
+        technologies: [
+            
+        ]
+    }
+];
+
+// project_name:"Solinsa Website",
+//             company_name: "Triple i soluciones",
+//             icon: "fa-solid fa-boxes-stacked"
+//         },
+//         {
+//             project_name:"Fluxy app",
+//             company_name: "Triple i soluciones",
+//             icon:"fa-regular fa-calendar-check"
+//         },
+//         {
+//             project_name:"Web portfolio",
+//             company_name:"School project",
+//             icon:"fa-solid fa-image"
+//         },
+//         {
+//             project_name:"Report system",
+//             company_name:"School project",
+//             icon:"fa-solid fa-chart-column"
+//         }
